ed44b7a11150a7b173977a84116b6734eaf9bce5
Binary-Quest <163310128+Binary-Quest@users.noreply.github.com>
2024-04-18 20:33:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "16575077"))
 API_HASH = os.environ.get("API_HASH", "1c8c0bcb55c14e0fd8078058966b6a11")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002022741169"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002047108967"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1702061654"))
